989ad93c3682b54fa73d7686538e0cc6704c4742
lukasz789 <lukaszjanyst@student.agh.edu.pl>
2026-07-23 22:49:31 +0200

    add s3 public access block and test object to S3 bucket

--- s3.tf
+++ s3.tf
@@ -5,3 +5,21 @@ resource "aws_s3_bucket" "main" {
     Name = "${var.project_name}-bucket"
   }
 }
+
+# New S3 buckets are private by default, but good practice to add it
+# -> this allows Terraform to detect/restore changes for any of these settings
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_object" "test_file" {
+  bucket       = aws_s3_bucket.main.id
+  key          = "test-file.txt"
+  content      = "Hello from the private S3 bucket."
+  content_type = "text/plain"
+}
