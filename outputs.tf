800d5f67f5afb4a3b46b0d0d9226cea2fec499ed
lukasz789 <lukaszjanyst@student.agh.edu.pl>
2026-07-25 15:38:25 +0200

    update tf variable example + add s3 to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "ec2_instance_id" {
   description = "ID of the EC2 instance."
   value       = aws_instance.private.id
 }
+
+output "s3_bucket_name" {
+  description = "Globally unique name generated for the S3 bucket."
+  value       = aws_s3_bucket.private.id
+}
